ee3d1b612d618aa14bfc3ac89d46991f5884f011
Antoine <lidurin.antoine@gmail.com>
2026-06-25 12:28:34 +0200

    config + date

---
+++ config/festivals.py
@@ -0,0 +1,45 @@
+FESTIVALS = [
+    ("Hellfest",                    "https://www.hellfest.fr",                    "Metal",           "Clisson"),
+    ("Rock en Seine",               "https://www.rockenseine.com",                "Rock",            "Saint-Cloud"),
+    ("Download Festival Paris",     "https://downloadfestival.fr",               "Metal/Rock",      "Paris"),
+    ("Eurockéennes de Belfort",     "https://www.eurockeennes.fr",               "Rock",            "Belfort"),
+    ("Cabaret Vert",                "https://www.cabaretvert.com",               "Rock/Alternatif", "Charleville-Mézières"),
+    ("Main Square Festival",        "https://www.mainsquarefestival.fr",         "Rock/Pop",        "Arras"),
+    ("Garorock",                    "https://www.garorock.com",                  "Rock/Hip-Hop",    "Marmande"),
+
+    ("Les Vieilles Charrues",       "https://www.vieillescharrues.asso.fr",      "Pop/Rock",        "Carhaix"),
+    ("Solidays",                    "https://www.solidays.org",                  "Électro/Pop",     "Paris"),
+    ("Lollapalooza Paris",          "https://www.lollapaloozafr.com",            "Pop/Rock/Électro","Paris"),
+    ("We Love Green",               "https://www.welovegreenmusic.com",          "Pop/Électro",     "Paris"),
+    ("Printemps de Bourges",        "https://www.printemps-bourges.com",         "Pop/Rock",        "Bourges"),
+    ("Musilac",                     "https://www.musilac.com",                   "Pop/Rock",        "Aix-les-Bains"),
+    ("Les Déferlantes",             "https://lesdeferlantes.com",                "Pop",             "Argelès-sur-Mer"),
+
+    ("Nuits Sonores",               "https://www.nuits-sonores.com",             "Électronique",    "Lyon"),
+    ("Peacock Society",             "https://www.peacocksociety.fr",             "Électronique",    "Paris"),
+    ("Delta Festival",              "https://deltafestival.com",                 "Électronique",    "Marseille"),
+    ("Marsatac",                    "https://www.marsatac.com",                  "Électronique",    "Marseille"),
+
+    ("Jazz in Marciac",             "https://www.jazzinmarciac.com",             "Jazz",            "Marciac"),
+    ("Jazz à Juan",                 "https://jazzajuan.com",                     "Jazz",            "Juan-les-Pins"),
+    ("Les Rendez-vous de l'Erdre",  "https://www.rendezvousdelerdre.com",        "Jazz/Classique",  "Nantes"),
+
+    ("Francofolies de La Rochelle", "https://www.francofolies.fr",               "Chanson/Pop FR",  "La Rochelle"),
+    ("Les Trans Musicales",         "https://www.lestrans.com",                  "Émergence",       "Rennes"),
+
+    ("Booska Summer Festival",      "https://www.booskasummerland.com",          "Hip-Hop",         "Ile-de-France"),
+
+    ("Festival Interceltique de Lorient", "https://festival-interceltique.bzh",  "Celtique",        "Lorient"),
+    ("Terres du Son",               "https://www.terresduson.com",               "Monde/Rock",      "Indre-et-Loire"),
+
+    ("Festival de Radio France",    "https://www.festival-radio-france.fr",      "Classique/Jazz",  "Montpellier"),
+    ("Chorégies d'Orange",          "https://www.choregies.fr",                  "Opéra",           "Orange"),
+
+    ("Reggae Sun Ska",              "https://www.reggaesunskaofficial.com",      "Reggae",          "Bordeaux"),
+
+    ("Art Rock",                    "https://www.artrock.fr",                    "Éclectique",      "Saint-Brieuc"),
+    ("Festival de Cornouaille",     "https://www.festival-cornouaille.bzh",      "Breton/Monde",    "Quimper"),
+    ("Beauregard Festival",         "https://www.beauregardfestival.com",        "Rock/Pop",        "Hérouville-Saint-Clair"),
+    ("Panoramas",                   "https://www.panoramas.fr",                  "Électro/Rock",    "Morlaix"),
+    ("Fête de l'Humanité",          "https://www.fetedelhumanite.fr",            "Éclectique",      "Plaine Saint-Denis"),
+]
